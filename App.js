f5bc58077359d7e277a8938f5c52136a058d60f3
alok verma <alokverma749@gmail.com>
2023-05-28 19:27:53 +0530

    ignited our app with pacel, removed cdn react links, add browserslist

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
-// Hello world using JS only
-const heading = document.createElement("h1");
-heading.innerHTML = "Hello world by JS";
-const ROOT1 = document.getElementById("root1");
-ROOT1.appendChild(heading);
-
-
-
-// Hello World using React CDN
+import React from "react"
+import ReactDOM from "react-dom/client";
 const DIV = React.createElement("div", { id: "parent" },
     React.createElement("div", { id: "child" },
         [React.createElement("h1", {}, "I am a child"),
         React.createElement("h2", {}, "I am second child")]
     ))
-const ROOT2 = ReactDOM.createRoot(document.getElementById("root"));
-ROOT2.render(DIV);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(DIV);
 
 
 
